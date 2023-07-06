9fa165c8a51a9e336cf21a5b74d9a54fb41f4895
Thomas Erdmenger <thomas.erdmenger@icloud.com>
2023-07-06 11:33:21 +0200

    Add effects on remaining keys

--- script.js
+++ script.js
@@ -4,6 +4,22 @@ const keyArr = document.querySelectorAll('.key')
 document.addEventListener('keydown', e => {
   if (e.code == 'KeyA') {
     keyArr[0].classList.add('keydown')
+  } else if (e.code == 'KeyS') {
+    keyArr[1].classList.add('keydown')
+  } else if (e.code == 'KeyD') {
+    keyArr[2].classList.add('keydown')
+  } else if (e.code == 'KeyF') {
+    keyArr[3].classList.add('keydown')
+  } else if (e.code == 'KeyG') {
+    keyArr[4].classList.add('keydown')
+  } else if (e.code == 'KeyH') {
+    keyArr[5].classList.add('keydown')
+  } else if (e.code == 'KeyJ') {
+    keyArr[6].classList.add('keydown')
+  } else if (e.code == 'KeyK') {
+    keyArr[7].classList.add('keydown')
+  } else if (e.code == 'KeyL') {
+    keyArr[8].classList.add('keydown')
   }
 })
 
@@ -11,5 +27,21 @@ document.addEventListener('keydown', e => {
 document.addEventListener('keyup', e => {
   if (e.code == 'KeyA') {
     keyArr[0].classList.remove('keydown')
+  } else if (e.code == 'KeyS') {
+    keyArr[1].classList.remove('keydown')
+  } else if (e.code == 'KeyD') {
+    keyArr[2].classList.remove('keydown')
+  } else if (e.code == 'KeyF') {
+    keyArr[3].classList.remove('keydown')
+  } else if (e.code == 'KeyG') {
+    keyArr[4].classList.remove('keydown')
+  } else if (e.code == 'KeyH') {
+    keyArr[5].classList.remove('keydown')
+  } else if (e.code == 'KeyJ') {
+    keyArr[6].classList.remove('keydown')
+  } else if (e.code == 'KeyK') {
+    keyArr[7].classList.remove('keydown')
+  } else if (e.code == 'KeyL') {
+    keyArr[8].classList.remove('keydown')
   }
 })
